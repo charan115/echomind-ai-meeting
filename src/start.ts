59d80ea5b0e97e97d5e3ec1825b40a145d86c8fe
GVC <charan2606kp@gmail.com>
2026-08-16 23:49:19 +0530

    Remove broken createMiddleware to fix server crash

--- src/start.ts
+++ src/start.ts
@@ -1,25 +1,7 @@
-import { createStart, createMiddleware } from "@tanstack/react-start";
-
-import { renderErrorPage } from "./lib/error-page";
+import { createStart } from "@tanstack/react-start";
 import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
-
-const errorMiddleware = createMiddleware().server(async ({ next }) => {
-  try {
-    return await next();
-  } catch (error) {
-    if (error != null && typeof error === "object" && "statusCode" in error) {
-      throw error;
-    }
-    console.error(error);
-    return new Response(renderErrorPage(), {
-      status: 500,
-      headers: { "content-type": "text/html; charset=utf-8" },
-    });
-  }
-});
 
 export const startInstance = createStart(() => ({
   functionMiddleware: [attachSupabaseAuth],
-  // We removed csrfMiddleware from this array so it stops crashing the server
-  requestMiddleware: [errorMiddleware], 
+  requestMiddleware: [], // Completely empty to stop the crash!
 }));
